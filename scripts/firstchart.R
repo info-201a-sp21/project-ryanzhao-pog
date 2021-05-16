4525be80b1517b2f3b096326fd0d2afe52372d60
rzhao4 <rzhao4@uw.edu>
2021-05-16 15:24:48 -0700

    finished geographical plot

--- scripts/firstchart.R
+++ scripts/firstchart.R
@@ -6,19 +6,43 @@ library("leaflet")
 
 # Read the data from the csv file
 hiv_df <- read.csv("../data/art_coverage_by_country_clean.csv",
-                   stringsAsFactors = FALSE, sep = ",")
+                   stringsAsFactors = FALSE)
 country_coordinates <- read.csv("../data/world_country_coordinate.csv", 
-                   stringsAsFactors = FALSE, sep = ",")
-View(hiv_df)
+                   stringsAsFactors = FALSE)
 
-combined_data <- merge(hiv_df, country_coordinates, by = "Country", all.x = TRUE)
+combined_data <- merge(hiv_df, country_coordinates, by = "Country", 
+                       all.x = TRUE)
 
-#Plot
-leaflet(data = combined_data) %>% 
+# Filtering Columns to show only countries with data in reported number of
+# people receiving ART.
+filtered_hiv <- filter(combined_data, Reported.number.of.people.receiving.ART 
+               != "Nodata")
+
+# Turning column in numeric values
+sapply(filtered_hiv, class)
+
+filtered_hiv$Reported.number.of.people.receiving.ART <-
+  as.numeric(filtered_hiv$Reported.number.of.people.receiving.ART)
+
+sapply(filtered_hiv, class)
+
+# Combining columns for information
+content <-  paste(sep = "<br/>",
+                  filtered_hiv$Country,
+                  filtered_hiv$Reported.number.of.people.receiving.ART)
+
+#Interactive Geographical Plot for Reported number of People Receiving Art
+geographic_plot_ART <- leaflet(data = filtered_hiv) %>%
   addProviderTiles("CartoDB.Positron") %>% 
   addCircles(
-    lat = ~Latitude,
     lng = ~Longitude,
-    stroke = FALSE,
-    popup = ~Reported.number.of.people.receiving.ART
+    lat = ~Latitude,
+    stroke = TRUE,
+    weight = 1,
+    fillOpacity = 0.3,
+    color = "Red",
+    radius = ~sqrt(Reported.number.of.people.receiving.ART) * 600,
+    popup = content
   )
+
+geographic_plot_ART
