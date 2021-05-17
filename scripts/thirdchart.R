5714728f25b84178b68261a655c2032da21158fd
Gerin George <gering92@uw.edu>
2021-05-17 16:04:49 -0700

    Added a function to take care of chart

--- scripts/thirdchart.R
+++ scripts/thirdchart.R
@@ -3,34 +3,29 @@ library("plotly")
 
 # Read the data from the csv file
 art_coverage_df <- read.csv("../data/art_coverage_by_country_clean.csv")
-colnames(art_coverage_df)
 
 # Isolate into columns that we are interested in
-data_for_plot <- art_coverage_df %>% 
+data_for_plotting <- function(dataset) {
+  dataset %>% 
   group_by(WHO.Region) %>% 
-  select(WHO.Region, Estimated.number.of.people.living.with.HIV_median) %>% 
-  summarize(total_median_num_HIV = 
-              sum(Estimated.number.of.people.living.with.HIV_median, 
-                  na.rm = TRUE))
-
-# Plot of the Estimated Number of People living with HIV per each WHO recognized
-# region of the world
-plot_hiv_per_region <- plot_ly(
-  data = data_for_plot,
-  x = ~WHO.Region,
-  y = ~total_median_num_HIV, 
-  type = "bar", 
-  name = "Number w/ HIV"
-) %>% 
-  layout(
-    title = "Estimated Number of People Living with HIV vs Region of the World",
-    xaxis = list(title = "Region of the World"),
-    yaxis = list(title = "Median Estimate of People Living with HIV"),
-    barmode = "group"
-  )
-
-plot_hiv_per_region
-
+    select(WHO.Region, Estimated.number.of.people.living.with.HIV_median) %>% 
+    new_df <- mutate(total_median_num_HIV = 
+                sum(Estimated.number.of.people.living.with.HIV_median, 
+                    na.rm = TRUE)) %>% 
+    plot_ly(
+      x = ~WHO.Region,
+      y = ~total_median_num_HIV, 
+      type = "bar", 
+      name = "Number w/ HIV"
+    ) %>% 
+      layout(
+        title = "Estimated Number of People Living with HIV vs Region of the World",
+        xaxis = list(title = "Region of the World"),
+        yaxis = list(title = "Median Estimate of People Living with HIV"),
+        barmode = "group"
+      )
+}
+data_for_plotting(art_coverage_df)
 # Description
 # This bar graph displays the median estimate of the number of people who are
 # living with HIV per WHO Region. The African region has by far the most number
